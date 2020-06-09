c5e52a0d04214e38975d871277a25442c41bc505
Dillon Coffman <tug40394@temple.edu>
2020-06-09 12:13:47 -0400

    Used custom Context component abstraction to provide instrument data to all Instrument related components; this separates the data from the component rendering the data

--- pages/instruments.js
+++ pages/instruments.js
@@ -4,15 +4,36 @@ import Menu from '../src/components/Menu/Menu'
 import Instruments from '../src/components/Instruments/Instruments'
 import InstrumentSearchBar from '../src/components/InstrumentSearchBar/InstrumentSearchBar'
 import Footer from '../src/components/Footer/Footer'
+import InstrumentContext from '../src/components/Instruments/InstrumentContext'
 
-const InstrumentsPage = () => (
-  <div>
-    <Header />
-    <Menu />
-    <InstrumentSearchBar />
-    <Instruments />
-    <Footer />
-  </div>
-);
+
+const InstrumentsPage = () => {
+  const instruments = [
+    {
+      imageSrc: 'guitars',
+      name: 'Guitars'
+    },
+    {
+      imageSrc: 'amp',
+      name: 'Amplifier'
+    },
+    {
+      imageSrc: 'drums',
+      name: 'Drums'
+    },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <InstrumentContext.Provider value={instruments}>
+        <InstrumentSearchBar />
+        <Instruments />
+      </InstrumentContext.Provider>
+      <Footer />
+    </div>
+  );
+};
 
 export default InstrumentsPage;
--- src/components/Instruments/Instruments.js
+++ src/components/Instruments/Instruments.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import InstrumentsRenderProps from './InstrumentsRenderProps'
+import React, { useContext } from 'react';
+import InstrumentContext from './InstrumentContext'
 
 const Instruments = () => {
+  const instruments = useContext(InstrumentContext);
+
   return (
-    <InstrumentsRenderProps>
-      {({ instruments }) => {
-        return (
-          <>
-            {instruments.map(({ imageSrc, name }) => {
-              return <img style={{ maxWidth: 400, width: 400 }} src={`/images/${imageSrc}.png`} alt={name} key={imageSrc} />
-            })}
-          </>
-        )
-      }}
-    </InstrumentsRenderProps>
-  )
-}
+    <>
+      {instruments.map(({ imageSrc, name }) => {
+        return <img style={{ maxWidth: 400, width: 400 }} src={`/images/${imageSrc}.png`} alt={name} key={imageSrc} />
+      })}
+    </>
+  );
+};
 
 export default Instruments;
